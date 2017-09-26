2f84c2021746c107f6b59d42f9205e8a0f64f211
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-09-26 14:18:03 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# deployment script run by Viper server after push
+
+echo "Starting deploy script"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+make install
+python $DIR/manage.py collectstatic --noinput
