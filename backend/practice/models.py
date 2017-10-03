cb5301a1fa29c73275e614e72fbb641be48edf27
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-10-03 16:56:21 +0200

    Add Block entity

--- backend/practice/models.py
+++ backend/practice/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Block(models.Model):
+    """Entity representing a programming block
+    """
+    name = models.CharField(max_length=256, unique=True)
+    order = models.SmallIntegerField()
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
