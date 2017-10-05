2b49c0eb1dd2385927d7464c1f4d78e637132019
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-10-05 21:35:21 +0200

    Add Django REST framework

--- backend/robomission/urls.py
+++ backend/robomission/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^practice/', include('practice.urls')),
+    url(r'^rest-api-auth/', include('rest_framework.urls')),
     url(r'^admin/', admin.site.urls),
 ]
