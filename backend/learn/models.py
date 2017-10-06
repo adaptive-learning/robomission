1f7b6537da0306c02dd314ef7b5aa5a79713bd79
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-10-06 09:44:10 +0200

    Add toolbox entity

--- backend/learn/models.py
+++ backend/learn/models.py
@@ -2,13 +2,23 @@ from django.db import models
 
 
 class Block(models.Model):
-    """Entity representing a programming block
+    """Entity for a programming block
     """
     name = models.CharField(max_length=256, unique=True)
     order = models.SmallIntegerField()
 
     class Meta:
         ordering = ['order']
+
+    def __str__(self):
+        return self.name
+
+
+class Toolbox(models.Model):
+    """Entity for a set of programming blocks
+    """
+    name = models.CharField(max_length=256, unique=True)
+    blocks = models.ManyToManyField(Block)
 
     def __str__(self):
         return self.name
