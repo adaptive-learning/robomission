019c6e96d10962bdfbb05990a943a2ad98c1c16d
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-10-07 19:49:28 +0200

    Add model for level

--- backend/learn/models.py
+++ backend/learn/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Block(models.Model):
-    """Entity for a programming block
+    """Programming block, such as "fly" or "repeat"
     """
     name = models.CharField(max_length=256, unique=True)
     order = models.SmallIntegerField()
@@ -16,13 +16,26 @@ class Block(models.Model):
 
 
 class Toolbox(models.Model):
-    """Entity for a set of programming blocks
+    """Set of programming blocks
     """
     name = models.CharField(max_length=256, unique=True)
     blocks = models.ManyToManyField(Block)
 
     def __str__(self):
         return self.name
+
+
+class Level(models.Model):
+    """Small group of coherent tasks of similar difficulty sharing same toolbox
+    """
+    level = models.SmallIntegerField()
+    name = models.SlugField(unique=True)
+    toolbox = models.ForeignKey(Toolbox)
+    credits = models.IntegerField(
+        help_text="Number of credits needed to complete this level.")
+
+    def __str__(self):
+        return 'L{level} {name}'.format(level=self.level, name=self.name)
 
 
 class Student(models.Model):
