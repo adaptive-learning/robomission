64521b62e14d684db0f02bd3b65bca7fe60ffe54
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-10-14 08:10:30 +0200

    Prefetch related entities when using world
    This eliminates number of DB queries when working with the world.
    For example, before this commit, task recommendation requried
    a query for each task to find its respective level entity.

--- backend/learn/world.py
+++ backend/learn/world.py
@@ -12,10 +12,29 @@ World = namedtuple('World', [
 
 
 def get_world(include=('blocks', 'toolboxes', 'levels', 'tasks', 'instructions')):
+    """Create a world of static entities.
+
+    Each attribute is a queryset with specified related entities prefetching.
+    """
     world = World(
-        blocks=Block.objects.all() if  'blocks' in include else None,
-        toolboxes=Toolbox.objects.all() if 'toolboxes' in include else None,
-        levels=Level.objects.all() if 'levels' in include else None,
-        tasks=Task.objects.all() if 'tasks' in include else None,
-        instructions=Instruction.objects.all() if 'instructions' in include else None)
+        blocks=(
+            Block.objects.all()
+            if 'blocks' in include
+            else None),
+        toolboxes=(
+            Toolbox.objects.prefetch_related('blocks').all()
+            if 'toolboxes' in include
+            else None),
+        levels=(
+            Level.objects.prefetch_related('tasks').all()
+            if 'levels' in include
+            else None),
+        tasks=(
+            Task.objects.select_related('level').all()
+            if 'tasks' in include
+            else None),
+        instructions=(
+            Instruction.objects.all()
+            if 'instructions' in include
+            else None))
     return world
