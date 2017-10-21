616f09c5373621406dee262bd60055856242de1a
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-10-21 21:08:53 +0200

    Define actions and action types

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,7 +1,19 @@
-import * as actionType from './types';
+import * as at from '../action-types';
 
-export const fetchWorld = () => {
-  return {
-    type: actionType.FETCH_WORLD_REQUESTED,
-  }
+function action(type, payload = {}) {
+  return {type, payload}
+}
+
+
+export const fetchWorld = {
+  //request: login => action(at.FETCH_WORLD_REQUEST),
+  success: (world) => action(at.FETCH_WORLD_SUCCESS, world),
+  failure: (error) => action(at.FETCH_WORLD_FAILURE, {error}),
+}
+
+
+export const fetchUser = {
+  request: login => action(at.FETCH_USER_REQUEST),
+  success: (user) => action(at.FETCH_USER_SUCCESS, user),
+  failure: (error) => action(at.FETCH_USER_FAILURE, {error}),
 }
