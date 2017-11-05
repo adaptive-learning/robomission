0be8bdac808b5bc1854361c07d238a3f07dd27ae
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-11-05 17:12:57 +0100

    Remove unused database setting

--- backend/robomission/settings.py
+++ backend/robomission/settings.py
@@ -75,12 +75,6 @@ WSGI_APPLICATION = 'robomission.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///{path}'.format(path=os.path.join(BASE_DIR, 'db.sqlite3')))
 }
