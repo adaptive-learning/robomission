5775719ff8e5de576581a0a53f3d5088aec01e92
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-11-10 09:32:07 +0100

    Change older migrations to avoid student-user problem
    This change requires to reset db and all migrations:
    $ ./backend/manage.py reset_db
    $ make db

--- backend/learn/migrations/0002_student_toolbox.py
+++ backend/learn/migrations/0002_student_toolbox.py
@@ -18,7 +18,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Student',
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('user', models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
                 ('credits', models.IntegerField(default=0)),
             ],
         ),
