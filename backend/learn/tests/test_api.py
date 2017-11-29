6f5434918eb2129712141023d314aa0807c13de9
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-11-29 16:26:40 +0100

    Add api tests for sign-up and login

--- backend/learn/tests/test_api.py
+++ backend/learn/tests/test_api.py
@@ -20,3 +20,43 @@ class StudentApiTestCase(APITestCase):
         self.assertQuerysetEqual(
             student.seen_instructions.all(),
             ['<Instruction: block.fly>'])
+
+
+class UserApiTestCase(APITestCase):
+    def test_succesful_registration(self):
+        data = {
+            'username': 'a@b.com',
+            'email': 'a@b.com',
+            'password1': 'robomise',
+            'password2': 'robomise'}
+        response = self.client.post(
+            '/rest-auth/registration/',
+            data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = User.objects.get(username='a@b.com')
+        self.assertEqual(user.email, 'a@b.com')
+
+    def test_succesful_login(self):
+        user = User.objects.create_user('a@b.com', 'a@b.com', 'robomise')
+        data = {
+            'username': 'a@b.com',
+            'email': 'a@b.com',
+            'password': 'robomise'}
+        response = self.client.post(
+            '/rest-auth/login/',
+            data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('key', response.data)
+
+    def test_unsuccesful_login(self):
+        data = {
+            'username': 'a@b.com',
+            'email': 'a@b.com',
+            'password': 'robomise'}
+        response = self.client.post(
+            '/rest-auth/login/',
+            data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
