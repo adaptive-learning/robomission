5015af127d444b94aebf9bd5ee3360d806d36b6b
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-11-30 14:27:40 +0100

    Create frontend authentication api

---
+++ frontend/src/authApi.js
@@ -0,0 +1,36 @@
+/**
+ * Defines API for authentication.
+ */
+import axios from 'axios';
+
+
+AUTH_API_PATH = '/rest-auth/';
+
+
+export function login(email, password) {
+  const url = `${AUTH_API_PATH}login`;
+  const data = {
+    username: email,
+    email,
+    password,
+  };
+  return axios.post(url, data);
+}
+
+
+export function logout() {
+  const url = `${AUTH_API_PATH}logout`;
+  return axios.post(url);
+}
+
+
+export function signUp(email, password) {
+  const url = `${AUTH_API_PATH}registration`;
+  const data = {
+    username: email,
+    email: email,
+    password1: password,
+    password2: password,
+  };
+  return axios.post(url, data);
+}
