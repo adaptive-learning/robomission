0ff1f8ce2b53edb0b55a4c4823caf05cbc16f42d
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-16 17:40:38 +0100

    Enable localized backend api

--- backend/robomission/settings.py
+++ backend/robomission/settings.py
@@ -73,6 +73,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
 import axiosDefaults from 'axios/lib/defaults';
 import { initGlobalBlockly } from './core/blockly';
 import { initGlobalTheme } from './theme';
+import { getLocale } from './localization';
 
 
 export const API_PATH = '/learn/api';
@@ -16,4 +17,5 @@ export function globalConfiguration() {
 function initGlobalAxios() {
   axiosDefaults.xsrfCookieName = 'csrftoken';
   axiosDefaults.xsrfHeaderName = 'X-CSRFToken';
+  axiosDefaults.headers.common['Accept-Language'] = getLocale();
 }
