f3f0894ecb2e432ba7863a9bb9a3d4c99f5d8a01
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-16 09:28:06 +0100

    Add tests for task weighting

---
+++ backend/learn/tests/test_recommendation.py
@@ -0,0 +1,43 @@
+import pytest
+
+from learn.models import Task, Level
+from learn.recommendation import _exponentially_weighted_tasks
+
+
+def level(lvl):
+    return Level(level=lvl, name='a-level')
+
+def task_at_level(lvl):
+    task = Task(name='a-task', level=level(lvl))
+    return task
+
+
+def test_exponentially_weighted_tasks__empty_list():
+    weighted_tasks = _exponentially_weighted_tasks([], preferred_level=5)
+    assert weighted_tasks == []
+
+
+def test_exponentially_weighted_tasks__prefer_preferred_level():
+    tasks = [task_at_level(3), task_at_level(4), task_at_level(5)]
+    weighted_tasks = _exponentially_weighted_tasks(tasks, preferred_level=4)
+    weights = [weight for _task, weight in weighted_tasks]
+    assert weights[1] > weights[0]
+    assert weights[1] > weights[2]
+
+
+def test_exponentially_weighted_tasks__decay_factor():
+    tasks = [task_at_level(3), task_at_level(4), task_at_level(5)]
+    weighted_tasks = _exponentially_weighted_tasks(
+        tasks, preferred_level=5, decay_factor=0.9)
+    weight3, weight4, weight5 = [weight for _task, weight in weighted_tasks]
+    assert weight5 == 100
+    assert weight4 == 90
+    assert weight3 == 81
+
+
+def test_exponentially_weighted_tasks__always_positive_weight():
+    tasks = [task_at_level(3)]
+    weighted_tasks = _exponentially_weighted_tasks(
+        tasks, preferred_level=10, decay_factor=0.1)
+    weight = weighted_tasks[0][1]
+    assert weight > 0
