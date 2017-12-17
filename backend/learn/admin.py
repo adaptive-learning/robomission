ce85c91fbf91bc4122aa1b4d65af28a4a0794362
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-17 08:07:52 +0100

    Allow to select records by date hierarchy in admin

--- backend/learn/admin.py
+++ backend/learn/admin.py
@@ -4,13 +4,30 @@ from django.contrib import admin
 from learn.models import Block, Toolbox, Level, Task, Instruction, Student
 from learn.models import TaskSession, ProgramSnapshot, Action, Feedback
 
+
+@admin.register(TaskSession)
+class TaskSessionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'start'
+
+
+@admin.register(ProgramSnapshot)
+class ProgramSnapshotAdmin(admin.ModelAdmin):
+    date_hierarchy = 'time'
+
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'time'
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    date_hierarchy = 'inserted'
+
+
 admin.site.register(Block)
 admin.site.register(Toolbox)
 admin.site.register(Level)
 admin.site.register(Task)
 admin.site.register(Instruction)
 admin.site.register(Student)
-admin.site.register(TaskSession)
-admin.site.register(ProgramSnapshot)
-admin.site.register(Action)
-admin.site.register(Feedback)
