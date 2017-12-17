6c470536bbd9e18cf3d6616a00d775dcd41a9917
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-17 09:15:15 +0100

    Add simple filters to admin

--- backend/learn/admin.py
+++ backend/learn/admin.py
@@ -15,31 +15,40 @@ class LevelAdmin(admin.ModelAdmin):
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'level')
     list_display_links = ('id', 'name')
+    list_filter = ('level',)
+
+
+@admin.register(Toolbox)
+class ToolboxAdmin(admin.ModelAdmin):
+    list_filter = ('blocks',)
 
 
 @admin.register(TaskSession)
 class TaskSessionAdmin(admin.ModelAdmin):
     list_display = ('id', 'student', 'task', 'solved', 'start', 'end')
     date_hierarchy = 'start'
+    list_filter = ('task', 'solved', 'start')
 
 
 @admin.register(ProgramSnapshot)
 class ProgramSnapshotAdmin(admin.ModelAdmin):
     list_display = ('id', 'task_session', 'program', 'granularity', 'correct')
     date_hierarchy = 'time'
+    list_filter = ('granularity', 'correct')
 
 
 @admin.register(Action)
 class ActionAdmin(admin.ModelAdmin):
     date_hierarchy = 'time'
+    list_filter = ('name',)
 
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     date_hierarchy = 'inserted'
+    list_filter = ('inserted', 'url')
 
 
 admin.site.register(Block)
-admin.site.register(Toolbox)
 admin.site.register(Instruction)
 admin.site.register(Student)
