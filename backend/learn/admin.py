860ea2ca54ea0d66613d2d38b19ee30bf059a141
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-17 07:45:44 +0100

    Register new models to admin site

--- backend/learn/admin.py
+++ backend/learn/admin.py
@@ -2,6 +2,7 @@
 """
 from django.contrib import admin
 from learn.models import Block, Toolbox, Level, Task, Instruction, Student
+from learn.models import TaskSession, ProgramSnapshot, Action, Feedback
 
 admin.site.register(Block)
 admin.site.register(Toolbox)
@@ -9,3 +10,7 @@ admin.site.register(Level)
 admin.site.register(Task)
 admin.site.register(Instruction)
 admin.site.register(Student)
+admin.site.register(TaskSession)
+admin.site.register(ProgramSnapshot)
+admin.site.register(Action)
+admin.site.register(Feedback)
