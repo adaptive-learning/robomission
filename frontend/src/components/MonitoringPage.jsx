6af83100d9bf95594eaaa3afde0d36a076e8545b
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-23 12:37:30 +0100

    Move theming from MonitoringPage container to component

--- frontend/src/components/MonitoringPage.jsx
+++ frontend/src/components/MonitoringPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 
-export default function MonitoringPage({ metrics }) {
-  return (
-    <div>
+class MonitoringPage extends React.Component {
+
+  renderActiveUsers() {
+    return (
       <Card style={{ margin: 10 }}>
         <CardTitle
           title="Active Users"
@@ -14,8 +16,23 @@ export default function MonitoringPage({ metrics }) {
           TBA
         </CardText>
       </Card>
-    </div>
-  );
+    );
+  }
+
+  render() {
+    const { metrics, muiTheme } = this.props;
+    // TODO: factor out longPageStyle (used in other pages as well)
+    const longPageContentStyle = {
+      maxWidth: 1200,
+      margin: '20px auto',
+      backgroundColor: muiTheme.palette.canvasColor,
+    };
+    return (
+      <div style={longPageContentStyle}>
+        {this.renderActiveUsers()}
+      </div>
+    );
+  }
 }
 
 MonitoringPage.propTypes = {
@@ -24,3 +41,6 @@ MonitoringPage.propTypes = {
 
 MonitoringPage.defaultProps = {
 };
+
+MonitoringPage = muiThemeable()(MonitoringPage);
+export default MonitoringPage;
--- frontend/src/containers/MonitoringPage.jsx
+++ frontend/src/containers/MonitoringPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import muiThemeable from 'material-ui/styles/muiThemeable';
 import MonitoringPageComponent from '../components/MonitoringPage';
 
 
@@ -14,21 +13,12 @@ const actionCreators = {
 
 class MonitoringPage extends React.Component {
   render() {
-    // TODO: move styling to a component
-    const longPageContentStyle = {
-      maxWidth: 1200,
-      margin: '20px auto',
-      backgroundColor: this.props.muiTheme.palette.canvasColor,
-    };
     return (
-      <div style={longPageContentStyle}>
-        <MonitoringPageComponent />
-      </div>
+      <MonitoringPageComponent />
     );
   }
 }
 
-MonitoringPage = muiThemeable()(MonitoringPage);
 MonitoringPage = connect(getProps, actionCreators)(MonitoringPage);
 
 export default MonitoringPage;
