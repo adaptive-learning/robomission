f4b9c8453af2aa5b7c688cf848a8a99d4895ab59
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-12-25 12:55:49 +0100

    Add solving-hours metric

--- backend/monitoring/metrics.py
+++ backend/monitoring/metrics.py
@@ -32,10 +32,57 @@ def to_timezone_aware(date, last_second=False):
     return aware_datetime
 
 
+def dates_range(first_date, last_date):
+    n_days = (last_date - first_date).days + 1
+    dates = [first_date + timedelta(days=d) for d in range(n_days)]
+    return dates
+
+
+def group_by_date(task_sessions):
+    groups = defaultdict(list)
+    for ts in task_sessions:
+        groups[ts.date].append(ts)
+    return groups
+
+
+def generate_active_students_metric(task_sessions, dates):
+    """Yield active-students metric for each date in dates.
+
+    Active student = has solve at least 1 task (in given day).
+    """
+    solved_task_sessions = [ts for ts in task_sessions if ts.solved]
+    groups = group_by_date(solved_task_sessions)
+    for date in dates:
+        n_active_students = len({ts.student_id for ts in groups[date]})
+        yield Metric(name='active-students', time=date, value=n_active_students)
+
+
+def generate_solved_tasks_metric(task_sessions, dates):
+    """Yield solved-tasks metric for each date in dates.
+    """
+    solved_task_sessions = [ts for ts in task_sessions if ts.solved]
+    groups = group_by_date(solved_task_sessions)
+    for date in dates:
+        n_solved_tasks = len(groups[date])
+        yield Metric(name='solved-tasks', time=date, value=n_solved_tasks)
+
+
+def generate_solving_hours_metric(task_sessions, dates):
+    """Yield solving-time metric for each date in dates.
+    """
+    solved_task_sessions = [ts for ts in task_sessions if ts.solved]
+    groups = group_by_date(solved_task_sessions)
+    hour = 3600
+    for date in dates:
+        total_solving_hours = sum(ts.time_spent for ts in groups[date]) / hour
+        yield Metric(name='solving-hours', time=date, value=total_solving_hours)
+
+
 class MetricsComputer:
     def __init__(self, first_date=None):
         self.first_date = first_date or get_first_unmeasured_date()
         self.last_date = get_yesterday()
+        self.dates = dates_range(self.first_date, self.last_date)
 
     def generate_and_save(self):
         metrics = self.compute()
@@ -48,32 +95,11 @@ class MetricsComputer:
         # previously computed metrics before they are replaced by the new
         # values.
         Metric.objects.filter(time__gte=self.first_date).delete()
-        # Daily Active Students = students who have solved at least 1 task
+        # Filter relevant entities
         time_range = (
             to_timezone_aware(self.first_date),
             to_timezone_aware(self.last_date, last_second=True))
-        task_sessions = (
-            TaskSession.objects
-            .annotate(date=Trunc('end', 'day'))
-            .filter(date__range=time_range))
-        dates_with_values = (
-            task_sessions
-            .filter(solved=True)
-            .values('date')
-            .annotate(
-                students=Count('student_id', distinct=True),
-                solved_tasks=Count('*')))
-        # TODO: Add solving-time metric. Note that SQLite cannot aggregate over
-        # times, so it's not enought to just annotate
-        # `solving_time=Sum(F('end') - F('start'))`.
-        # We use defaultdict to return 0 for dates missing in the aggregation.
-        values = defaultdict(int)
-        for group in dates_with_values:
-            date = group['date'].date()
-            values[(date, 'active-students')] = group['students']
-            values[(date, 'solved-tasks')] = group['solved_tasks']
-        n_days = (self.last_date - self.first_date).days + 1
-        dates = [self.first_date + timedelta(days=d) for d in range(n_days)]
-        for date in dates:
-            for name in ('active-students', 'solved-tasks'):
-                yield Metric(name=name, time=date, value=values[(date, name)])
+        task_sessions = list(TaskSession.objects.filter(end__date__range=time_range))
+        yield from generate_active_students_metric(task_sessions, self.dates)
+        yield from generate_solved_tasks_metric(task_sessions, self.dates)
+        yield from generate_solving_hours_metric(task_sessions, self.dates)
