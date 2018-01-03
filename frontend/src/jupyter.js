5a04fd99dcfa7860121598d5483908bf0f625829
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-01-03 23:01:22 +0100

    Don't minify JS for jupyter-react

--- frontend/src/jupyter.js
+++ frontend/src/jupyter.js
@@ -4,15 +4,8 @@
 import JupyterReact from 'jupyter-react-js';
 import components from './components';
 
-function load_ipython_extension () {
-  requirejs([
-    "base/js/namespace",
-    "base/js/events",
-  ], function( Jupyter, events ) {
-    JupyterReact.init( Jupyter, events, 'react.components', { components, save: false } );
+export function load_ipython_extension () {
+  requirejs(['base/js/namespace', 'base/js/events'], function(Jupyter, events) {
+    JupyterReact.init(Jupyter, events, 'react.components', {components, save: false});
   });
 }
-
-module.exports = {
-  load_ipython_extension: load_ipython_extension
-};
