505be6e24f03971646044a1a11586a2c4cae2ced
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-01-05 20:28:08 +0100

    Allow for evolving a react component inside jupyter notebook cell

--- visualization/space_world.py
+++ visualization/space_world.py
@@ -9,5 +9,34 @@ class SpaceWorld(Component):
         super().__init__(target_name='react.components', **kwargs)
         self.on_msg(self._handle_msg)
 
+    def set(self, world_description):
+        fields = parse_space_world(world_description)
+        self.send({'method': 'update', 'props': {'fields':  fields}})
+
     def _handle_msg(self, msg):
         print(msg)
+
+
+def parse_space_world(description):
+    """Transform text describing a SpaceWorld into a 2D array of fields.
+    """
+    description = ''.join(description.split())  # remove whitspace
+    rows = description.split('||')
+    tokenized_rows = [row.strip('|').split('|') for row in rows]
+    fields = [[parse_field(field) for field in row] for row in tokenized_rows]
+    return fields
+
+
+def parse_field(field):
+    """Transform string describing a field into a tuple (background, objects).
+
+    If no color is given, defaults to black.
+    """
+    background = 'k'
+    objects = []
+    for letter in field:
+        if letter.islower():
+            background = letter
+        else:
+            objects.append(letter)
+    return [background, objects]
