0475bade4571fa70050bd5a7c5d00c71ebb82b10
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-01-25 09:22:54 +0100

    Add tests for has_mastered

--- backend/learn/tests/test_mastery.py
+++ backend/learn/tests/test_mastery.py
@@ -1,6 +1,6 @@
 import pytest
 
-from learn.models import Student, Task, Chunk
+from learn.models import Student, Task, Chunk, TaskSession, Skill
 from learn.mastery import get_first_unmastered_chunk
 from learn.mastery import has_mastered
 
@@ -24,7 +24,38 @@ def chunk_with_1_task(task1):
     return chunk
 
 
-# Django DB is always needed form many-to-many relations (chunks.tasks)
+# Django DB is always needed for many-to-many relations (chunks.tasks)
 @pytest.mark.django_db
 def test_has_mastered__initially_not(initial_student, chunk_with_1_task):
-    assert has_mastered(initial_student, chunk_with_1_task) == False
+    assert not has_mastered(initial_student, chunk_with_1_task)
+
+
+# django db is always needed for many-to-many relations (student.skills)
+# todo: find a way how to test the following without using db.
+@pytest.mark.django_db
+def test_has_mastered__when_skill_is_1():
+    chunk = Chunk.objects.create(name='c1')
+    student = Student.objects.create()
+    Skill.objects.create(student=student, chunk=chunk, value=1.0)
+    assert has_mastered(student, chunk)
+
+
+@pytest.mark.django_db
+def test_has_mastered__not_when_skill_is_low():
+    chunk = Chunk.objects.create(name='c1')
+    student = Student.objects.create()
+    Skill.objects.create(student=student, chunk=chunk, value=0.5)
+    assert not has_mastered(student, chunk)
+
+
+#@pytest.mark.django_db
+#def test_has_mastered__excellent_performance():
+#    task1 = Task.objects.create(id=1, name='t1', setting='{}', solution='')
+#    task2 = Task.objects.create(id=2, name='t2', setting='{}', solution='')
+#    chunk = Chunk.objects.create(id=1, name='c1')
+#    chunk.tasks.set([task1, task2])
+#    # Id=1 already taken by the student "initial" created in a DB migration.
+#    student = Student.objects.create(id=2)
+#    ts = TaskSession.objects.create(
+#        student=student, task=task1, performance=TaskSession.EXCELLENT)
+#    assert has_mastered(student, chunk)
