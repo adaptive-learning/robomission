7298e8245ffd7cc293db30b3838c52a738ac7cce
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-05-25 10:15:36 +0200

    Complete data migration for skills

--- backend/learn/migrations/0055_data_skills.py
+++ backend/learn/migrations/0055_data_skills.py
@@ -1,5 +1,6 @@
 """Compute and store skills for the tasks solved in the past.
 """
+from collections import Counter
 from django.db import migrations
 
 
@@ -15,19 +16,24 @@ def compute_skills(apps, schema_editor):
     # by a simple approximation: if the student has solved any task from given
     # phase, the phase is solved (the corresponding skill is 1).
 
-    task_sessions = TaskSessions.objects.select_related('task__problemset').all()
+    task_sessions = TaskSession.objects.select_related('task__problemset').all()
 
     # Dict is used to remove duplicates (Set cannot be used, because primary
     # keys would be used to compare the skills, which are not set yet.)
-    phase_skills = list({
+    phase_skills = set({
         (ts.student.pk, ts.task.pk):
             Skill(student=ts.student, chunk=ts.task.problemset, value=1)
         for ts in task_sessions}.values())
 
-    # TODO:Add skills for missions.
-    # ... count unique by (student.pk, skill.chunk.problemset.parent_mission.pk)
-    # mission_skills = ...
+    # Add skills for missions.
+    solved_phases_counts = Counter(
+        (skill.student.pk, skill.chunk.problemset.parent_mission.pk)
+        for skill in phase_skills)
+    mission_skills = {
+        Skill(student=student_pk, chunk=mission_pk, value=count/3)
+        for (student_pk, mission_pk), count in solved_phases_counts.items()}
 
+    skills = phase_skills | mission_skills
     Skill.objects.bulk_create(skills)
 
 
