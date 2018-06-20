9c463dc16057086053939cfb1f6fea2c2eb4f1b9
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-06-20 14:29:04 +0200

    Finalize program snapshot export test

--- backend/learn/tests/test_export.py
+++ backend/learn/tests/test_export.py
@@ -9,9 +9,7 @@ from learn.models import Student, Task, TaskSession, ProgramSnapshot
 @pytest.mark.django_db
 def test_export_is_fast_enough():
     # Create a lot of ProgramSnapshots
-    #n_snapshots= 500000
-    #n_snapshots= 200000
-    n_snapshots= 200
+    n_snapshots = 100000
     s = Student.objects.create()
     task = Task.objects.create()
     ts = TaskSession.objects.create(student=s, task=task)
